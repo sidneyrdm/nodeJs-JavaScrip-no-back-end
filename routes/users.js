d88004e825d793d9ca64a4354f5f8fdd545f38d8
sidneyrdm <sidneybritomal@gmail.com>
2021-07-29 01:08:45 -0300

    implementação dos métodos PUT e DELETE

--- routes/users.js
+++ routes/users.js
@@ -31,7 +31,6 @@ let route = app.route('/users');
     
     route.post((req, res)=>{
         
-        
         db.insert(req.body, (err, user)=> {
             if(err){
                 app.utils.error.send(err, req, res);
@@ -53,4 +52,22 @@ let route = app.route('/users');
         });
     });
     
+    routeId.put((req, res)=> {
+        db.update({_id:req.params.id}, req.body, err => {
+            if(err){
+                app.utils.error.send(err, req, res);
+            }else{
+                res.status(200).json(Object.assign(req.params,req.body));
+            }
+        });
+    });
+    routeId.delete((req, res)=> {
+        db.remove({_id:req.params.id}, {}, err => {
+            if(err){
+                app.utils.error.send(err, req, res);
+            }else{
+                res.status(200).json(req.params);
+            }
+        });
+    });
 };
